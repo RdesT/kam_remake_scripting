4d69ebefd510956485202fc3af8c49e45af015a5
Ritter des Todes <Ritter.des.Todes@mail.ru>
2024-12-18 15:06:11 +0300

    Removed CommandLineParser. We want to keep 1 exe file and not use dll if it's not important.

--- Tools/BuildingHelperTools/BuildingHelperFilesCreator/BuildingHelperFilesCreatorInstaller/Program.cs
+++ Tools/BuildingHelperTools/BuildingHelperFilesCreator/BuildingHelperFilesCreatorInstaller/Program.cs
@@ -1,5 +1,4 @@
-﻿using CommandLine;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -12,51 +11,54 @@ namespace BuildingHelperFilesCreatorInstaller
 {
 	class Program
 	{
-		public class Options
-		{
-			[Option("latestExePath", Required = true, HelpText = "Path to latest executable that was downloaded")]
-			public string LatestExePath { get; set; }
-			[Option("appExePath", Required = true, HelpText = "Path to application where we want to update executable.")]
-			public string AppExePath { get; set; }
-		}
-
 		static void Main(string[] args)
 		{
 			Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-			Parser.Default.ParseArguments<Options>(args).WithParsed(opts =>
+			var latestExePath = string.Empty;
+			var appExePath = string.Empty;
+
+			for (int i = 0; i < args.Length; i++)
 			{
-				if (string.IsNullOrWhiteSpace(opts.LatestExePath))
-					throw new Exception(
-						"Wrong arguments! Check latestExePath parameter!");
-				if (string.IsNullOrWhiteSpace(opts.AppExePath))
-					throw new Exception(
-						"Wrong arguments! Check appExePath parameter!");
-				
-				var processInfo = new ProcessStartInfo()
+				if (args[i] == "-latestExePath")
 				{
-					FileName = opts.AppExePath,
-					WorkingDirectory = new FileInfo(opts.AppExePath).Directory.FullName,
-				};
-				
-				try
-				{
-					//We wait for main application to be closed.
-					Task.Delay(1500);
-
-					File.Copy(opts.LatestExePath, opts.AppExePath, true);
-
-					processInfo.Arguments = "";
-
-					Process.Start(processInfo);
-				}		
-				catch
-				{
-					//Starting process once again if we failed to update for some reason.
-					processInfo.Arguments = "";
-					Process.Start(processInfo);
+					latestExePath = args[i + 1];
 				}
-			});			
+				if (args[i] == "-appExePath")
+				{
+					appExePath = args[i + 1];
+				}
+			}
+
+			if (string.IsNullOrEmpty(latestExePath) || string.IsNullOrEmpty(appExePath))
+			{
+				Console.WriteLine("Wrong arguments");
+				return;
+			}
+
+			var processInfo = new ProcessStartInfo()
+			{
+				FileName = appExePath,
+				WorkingDirectory = new FileInfo(appExePath).Directory.FullName,
+			};
+
+			try
+			{
+				//We wait for main application to be closed.
+				Task.Delay(1500);
+
+				File.Copy(latestExePath, appExePath, true);
+
+				processInfo.Arguments = "";
+
+				Process.Start(processInfo);
+			}
+			catch
+			{
+				//Starting process once again if we failed to update for some reason.
+				processInfo.Arguments = "";
+				Process.Start(processInfo);
+			}
 		}
 	}
 }
